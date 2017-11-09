90eddc11c79751b23c342250e974c8390520896a
Aaron <asgaaron@gmail.com>
2017-11-08 19:45:02 -0700

    added prefabs, scripts, and setup test screen

---
+++ 2DPhysicsDemo/Assets/Scripts/PickUps.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickUps : MonoBehaviour {
+
+    public int value = 5;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        Debug.Log("Triggered!");
+        Destroy(gameObject);
+    }
+}
